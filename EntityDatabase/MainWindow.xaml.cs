2431a3f66f40367a719b7fb503cc63adffd26990
Timok Alekseev <timofey.alekseev@list.ru>
2024-03-30 08:34:56 +0300

    Добавьте файлы проекта.

---
+++ EntityDatabase/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EntityDatabase
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GameEntities context = new GameEntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Weapons.ItemsSource = context.Weapon.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Pageframe1.Content = new WeaponsTypesPage();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Pageframe2.Content = new PlayersPage();
+
+        }
+    }
+}
---
+++ EntityDatabase/PlayersPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EntityDatabase
+{
+    /// <summary>
+    /// Логика взаимодействия для PlayersPage.xaml
+    /// </summary>
+    public partial class PlayersPage : Page
+    {
+        private GameEntities context = new GameEntities();
+
+        public PlayersPage()
+        {
+            InitializeComponent();
+            PlayersGrid.ItemsSource = context.Players.ToList();
+
+
+        }
+
+
+    }
+}
